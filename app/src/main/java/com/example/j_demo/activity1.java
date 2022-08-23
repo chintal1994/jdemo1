96932ffa838f608833a1cc499db1f6e6c029a79b
Chintal Pankajkumar Patel <chintal_patel9494@yahoo.com>
2022-08-23 12:54:57 +0530

    commit3

--- app/src/main/java/com/example/j_demo/activity1.java
+++ app/src/main/java/com/example/j_demo/activity1.java
@@ -1,6 +1,13 @@
 package com.example.j_demo;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
